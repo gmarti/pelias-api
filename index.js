9fe77f0416417cadc89d5aca05ff4a33b2bc50d2
Grégory marti <greg.marti@gmail.com>
2024-09-04 13:05:46 +0200

    Add option for keep alive timeout

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const type_mapping = require('./helper/type_mapping');
 
 const app = require('./app'),
   port = (process.env.PORT || 3100),
-  host = (process.env.HOST || undefined);
+  host = (process.env.HOST || undefined),
+  keepAliveTimeout = Number(process.env.KEEP_ALIVE_TIMEOUT || '5000');
 
 let server;
 
@@ -14,6 +15,7 @@ type_mapping.load(() => {
     const listenAddress = server.address();
     logger.info(`pelias is now running on http://${listenAddress.address}:${listenAddress.port}`);
   });
+  server.keepAliveTimeout = keepAliveTimeout;
 });
 
 function exitHandler() {
